0fc04da5b02eb6d16427d4e1e14bcbeb63372731
Noel Kwan <noelkwan1998@gmail.com>
2021-11-26 16:06:25 +0800

    Add interaction outline

--- index.js
+++ index.js
@@ -63,53 +63,108 @@ const scriptPathTable = {
 // This is a demo script for running an auction in Nervos under high contention.
 async function main()
 {
-	// Initialize the Lumos configuration which is held in config.json.
+	// Initialize the Lumos Indexer
+    const indexer = await initializeLumos();
+
+    // Deploy Code cells
+    // scriptMetaTable is a map between script names (see Config section above),
+    // and their metadata (outpoints, codehash).
+    const scriptMetaTable = await createCodeCells(indexer)
+    const { codehash: noopCodeHash
+          , outpoint: noopOutpoint } = scriptMetaTable[AUCTION_NOOP_LOCK_SCRIPT]
+
+    // Creates the initial asset cell, owned by the seller.
+    const assetOutpoint = await createAssetCell(indexer, noopCodeHash, noopOutpoint)
+
+    // Creates auction state cells:
+    // - Auction consensus: state of auction
+    // - Auction escrow: holds the assets.
+    const auctionTx0 =
+          await createAuctionCells(indexer, assetOutpoint, noopOutpoint, noopCodeHash)
+
+    // Initial auction bid
+    const auctionTx1 =
+          await placeBid(indexer, 10, auctionTx0)
+
+    // Failed bid
+    // NOTE: With rebase script it should pass through.
+    const auctionTx3 =
+          await placeBid(indexer, 100, auctionTx0)
+
+    // Collate and describe auction end state
+    // TODO: Change these to reference outpoint2 namespace.
+    // after rebase has been integrated into RPC and syscalls.
+    // const currentBidCellData = fetchCurrentBidData(indexer, auctionTx3)
+
+    // printBidCellData(current_bid)
+
+    headerLog("DONE")
+}
+
+// ----------- Internals
+
+async function initializeLumos() {
+    // configuration which is held in config.json.
 	initializeConfig();
 
 	// Start the Lumos Indexer and wait until it is fully synchronized.
-	header_log("Initializing Indexer")
+	headerLog("Initializing Indexer")
 	const indexer = await initializeLumosIndexer(DEFAULT_NODE_URL);
-	header_log("Initialized Indexer")
-
-    // Deploy code cells
-    header_log("Deploying all code cells")
-    const scriptMetaTable = await createCodeCells(indexer) // Returns metadata of scripts (outpoints, script hash)
-                                                     // Which are used in later steps
-    header_log("Deployed all code cells")
-    console.log("Script Metadata:")
-    console.debug(JSON.stringify(scriptMetaTable, null, 2))
-
-    console.debug("==== Deploying asset cells")
-    const { codehash: noopScriptHash
-          , outpoint: noopOutpoint } = scriptMetaTable[AUCTION_NOOP_LOCK_SCRIPT]
-    const assetOutpoint = await createAssetCell(indexer, noopScriptHash, noopOutpoint)
-    console.debug("==== Deployed asset cells")
-
-    // console.debug("==== Deploying initial contract state cells")
-    // let {consensusOutpoint, assetsOutpoint} =
-    //     createInitialContractStateCells(indexer, outpoints)
-    // console.debug("==== Deployed initial contract state cells")
-
-    header_log("DONE")
+	headerLog("Initialized Indexer")
+    return indexer
 }
 
+async function fetchCurrentBid(indexer, consensusOutpoint, escrowOutpoint, bidOutpoint) {
+    const currentBidOutpoint =
+          await fetchCurrentBidOutpoint(indexer, consensusOutpoint, escrowOutpoint);
+    return currentBidOutpoint
+}
 
-// indexer: Use to find cells.
-// TODO: Replace noop lockscript with actual lockscript
-// noopScriptHash: Hash of noop script code
-// noopOutpoint: Outpoint of noop lockscript, for use after code cell deployed.
-async function createAssetCell(indexer, noopScriptHash, noopOutpoint) {
-    	// Create a transaction skeleton.
-	let transaction = TransactionSkeleton({cellProvider: indexer});
+async function fetchCurrentBidOutpoint(indexer, consensusOutpoint, escrowOutpoint) {
+    await syncIndexer(indexer)
+    headerLog("Fetching bid outpoint")
+    headerLog("Fetched bid outpoint")
+    return null
+}
 
-	// Add the cell deps for noop lock script
-	transaction = addDefaultCellDeps(transaction);
+// TODO: Consume assetOutpoint
+async function createAuctionCells(indexer, assetOutpoint, noopOutpoint, noopCodeHash) {
+    headerLog("Creating Auction Cells")
+    await syncIndexer(indexer)
+
+    let transaction = make_default_transaction(indexer);
     transaction = addCellDeps(transaction, {dep_type: "code", out_point: noopOutpoint})
 
-	// Create asset cell
-	const outputCapacity = ckbytesToShannons(1000n);
-	const lockScript = { args: "0x00", code_hash: noopScriptHash , hash_type: "data"}
-	const data = intToU128LeHexBytes(100n); // TODO: maybe use a proper asset?
+    // Create consensus cell
+    // TODO: This needs auction type script.
+    let consensusOutput = makeBasicCell(1000n, noopCodeHash)
+	transaction = transaction.update("outputs", (i)=>i.push(consensusOutput));
+
+    let escrowOutput = makeBasicCell(1000n, noopCodeHash)
+	transaction = transaction.update("outputs", (i)=>i.push(escrowOutput));
+
+    transaction = await balanceCapacity(GENESIS_ADDRESS, indexer, transaction)
+
+	// Add in the witness placeholders.
+	transaction = addDefaultWitnessPlaceholders(transaction);
+
+    const { tx_hash } = await fulfillTransaction(transaction);
+
+    // NOTE: This is left out, because it is expected encoding,
+    // the type script will perform a check to ensure the tx
+    // is in this exact format,
+    // hence we can just grab the tx hash, and expect that its outpoints
+    // will be indexed as such:
+    // const consensusOutpoint = { tx_hash, index: "0x00" } // TODO: Is this the correct consensus outpoint???
+    // const escrowOutpoint = { tx_hash, index: "0x01" } // TODO: Is this correct format??? hmm...
+    headerLog("Created Auction Cells")
+    return tx_hash
+}
+
+function makeBasicCell(amount, scriptHash) {
+    const outputCapacity = ckbytesToShannons(1000n);
+	const lockScript = { args: "0x00", code_hash: scriptHash , hash_type: "data"}
+	const data = intToU128LeHexBytes(100n); // TODO: Construct the entire JSON of the consensus cell.
 	const output =
     { cell_output:
       { capacity: intToHex(outputCapacity)
@@ -117,6 +172,35 @@ async function createAssetCell(indexer, noopScriptHash, noopOutpoint) {
       }
     , data: data
     };
+    return output
+}
+// TODO: Once rebase script is in, this should call `send_transaction`,
+// with the rebase_script and cell indices parameters.
+// TODO: We have elided the keypair argument you see in the test-suite for place_bid.
+// This is needed in the real auction, in order to construct bid and refund lock scripts.
+// Since we noop everything for contention PoC, this isn't an issue here.
+async function placeBid(indexer, amount, auctionTxHash) {
+    headerLog("Placing bid")
+    await syncIndexer(indexer)
+    const consensusOutpoint = null
+    const escrowOutpoint = null
+    headerLog("Placed bid")
+    return "0x1234"
+}
+
+// indexer: Use to find cells.
+// TODO: Replace noop lockscript with actual lockscript (auction-escrow-lock)
+// noopScriptHash: Hash of noop script code
+// noopOutpoint: Outpoint of noop lockscript, for use after code cell deployed.
+async function createAssetCell(indexer, noopCodeHash, noopOutpoint) {
+    console.debug("==== Deploying asset cells")
+    await syncIndexer(indexer)
+
+    let transaction = make_default_transaction(indexer);
+    transaction = addCellDeps(transaction, {dep_type: "code", out_point: noopOutpoint})
+
+	// Create asset cell
+	const output = makeBasicCell(1000n, noopCodeHash)
 	transaction = transaction.update("outputs", (i)=>i.push(output));
 
     transaction = await balanceCapacity(GENESIS_ADDRESS, indexer, transaction)
@@ -125,75 +209,9 @@ async function createAssetCell(indexer, noopScriptHash, noopOutpoint) {
 	transaction = addDefaultWitnessPlaceholders(transaction);
 
     const outpoint = await fulfillTransaction(transaction);
+    console.debug("==== Deployed asset cells")
     return outpoint
 }
-
-// async function createInitialContractStateCells(indexer, outpoints) {
-
-//     // Must wait for indexer to sync
-//     await syncIndexer(indexer)
-
-// 	// Create a transaction skeleton.
-// 	let transaction = TransactionSkeleton({cellProvider: indexer});
-
-// 	// Add the cell deps:
-//     // TODO: What scripts do we need???
-// 	transaction = addDefaultCellDeps(transaction);
-// 	const cellDep = {dep_type: "code", out_point: scriptCodeOutPoint};
-// 	transaction = transaction.update("cellDeps", (cellDeps)=>cellDeps.push(cellDep));
-
-// 	// Create asset cells
-// 	const outputCapacity = ckbytesToShannons(1000n);
-// 	const lockScript = null; // TODO: escrow lockscript
-//     const typeScript = null; // TODO: auction typescript.
-// 	// const typeScript1 =
-// 	// 	{
-// 	// 		code_hash: dataFileHash1,
-// 	// 		hash_type: "data",
-// 	// 		args: lockScriptHashAlice
-// 	// 	};
-// 	// 	const data1 = intToU128LeHexBytes(addressTokenPair[1]);
-// 	const output = {cell_output: {capacity: intToHex(outputCapacity), lock: lockScript, type: typeScript}, data: data};
-// 	transaction = transaction.update("outputs", (i)=>i.push(output));
-
-// 	// Determine the capacity from all output Cells.
-// 	const outputCapacity = transaction.outputs.toArray().reduce((a, c)=>a+hexToInt(c.cell_output.capacity), 0n);
-
-// 	// Add input capacity cells.
-// 	const capacityRequired = outputCapacity + ckbytesToShannons(61n) + DEFAULT_TX_FEE;
-// 	const collectedCells = await collectCapacity(indexer, addressToScript(ALICE_ADDRESS), capacityRequired);
-// 	transaction = transaction.update("inputs", (i)=>i.concat(collectedCells.inputCells));
-
-// 	// Determine the capacity of all input cells.
-// 	const inputCapacity = transaction.inputs.toArray().reduce((a, c)=>a+hexToInt(c.cell_output.capacity), 0n);
-
-// 	// Create a change Cell for the remaining CKBytes.
-// 	const changeCapacity = intToHex(inputCapacity - outputCapacity - DEFAULT_TX_FEE);
-// 	let change = {cell_output: {capacity: changeCapacity, lock: addressToScript(ALICE_ADDRESS), type: null}, data: "0x"};
-// 	transaction = transaction.update("outputs", (i)=>i.push(change));
-
-// 	// Add in the witness placeholders.
-// 	transaction = addDefaultWitnessPlaceholders(transaction);
-
-// 	// Print the details of the transaction to the console.
-// 	describeTransaction(transaction.toJS());
-
-// 	// Validate the transaction against the lab requirements.
-// 	await validateLab(transaction, "create");
-
-// 	// Sign the transaction.
-// 	const signedTx = signTransaction(transaction, ALICE_PRIVATE_KEY);
-
-// 	// Send the transaction to the RPC node.
-// 	const txid = await sendTransaction(DEFAULT_NODE_URL, signedTx);
-// 	console.log(`Transaction Sent: ${txid}\n`);
-
-// 	// Wait for the transaction to confirm.
-// 	await waitForTransactionConfirmation(DEFAULT_NODE_URL, txid);
-// 	console.log("\n");
-// }
-
-// ----------- Internals
 
 // indexer: [indexer] - lumos indexer instance
 // returns: Map scriptName { outpoint: [Outpoint], codehash: [CodeHash] }
@@ -201,7 +219,9 @@ async function createAssetCell(indexer, noopScriptHash, noopOutpoint) {
 //               , ...
 //               }
 const createCodeCells = async (indexer) => {
-     const scriptMetaTable = {};
+    headerLog("Deploying all code cells")
+
+    const scriptMetaTable = {};
 
     // deploy all code cells.
     for (const [scriptName, path] of Object.entries(scriptPathTable)) {
@@ -210,6 +230,11 @@ const createCodeCells = async (indexer) => {
         scriptMetaTable[scriptName] = metadata
         console.log("Deployed code cell: ", scriptName)
     }
+
+    headerLog("Deployed all code cells")
+    console.log("Script Metadata:")
+    console.debug(JSON.stringify(scriptMetaTable, null, 2))
+
     return scriptMetaTable
 }
 
@@ -318,7 +343,7 @@ const balanceCapacity = async (input_cells_address, indexer, transaction) => {
 
 // ----------- Utilities
 
-const header_log = (s) => {
+const headerLog = (s) => {
     console.debug("\n" + "====" + s + "\n")
 }
 
